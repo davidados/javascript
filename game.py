f19f2efd215b75480bb8f9fbfe9143cbd227789a
David Varga <david.varga92@gmail.com>
2020-06-30 10:56:04 +0200

    first commit

---
+++ game.py
@@ -0,0 +1,42 @@
+import time
+import sys
+import random
+
+item = ""
+equipped_item = ""
+items = ["key", "ticket"]
+
+
+
+def print_pause(message):
+    print(message)
+    time.sleep(2)
+
+def game_over():
+    print("game is over")
+
+def intro():
+    print_pause("It's summer, the temperature is unbearable, \n"
+                "there is no air conditioner in your flat.")
+    print_pause("The internet has gone, some COVID rules are still in effect." )
+    print_pause("Furthermore, it's TW week, so you deciced to visit Coodcool's headquarter phisically.")
+    print_pause("This is how an epic journey started.")
+
+     
+def location_flat():
+    print_pause("It's morning, you already showered and dressed up for the departure, \n
+    "although you dont find your keys." )
+    print_pause("What do you do?
+    print_pause("A.) Lay back, and tell Coodcool that you are sick.)
+    print_pause("B.) Start looking for your keys")
+    print_pause("Please press A or B")
+
+
+
+
+
+
+
+
+
+intro()
